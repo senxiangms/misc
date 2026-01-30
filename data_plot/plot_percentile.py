f1af838db5e5caee3d9849985888b3c8b64083cc
senxiangms <sxiang@PR-SXIANG.local>
2026-01-30 10:21:47 -0800

    add plot_percentile

---
+++ data_plot/plot_percentile.py
@@ -0,0 +1,75 @@
+# parse from log file like: 
+# test_util.py(  354): INFO       : idx=13: sim=0.724 ttf=0.49 secs, time=4.14 secs, ntok=(782, 582) itok/sec=1586.07, otok/sec=159.71, otok/rtsec=140.68, response=<Response [200 OK]>, timeout=20.0
+# get idx, sim, ttf, time, ntok, itok/sec, otok/sec, otok/rtsec
+
+import pandas as pd
+import matplotlib.pyplot as plt
+from collections import defaultdict
+import numpy as np
+
+def get_metrics_from_log_file(log_file):
+    with open(log_file, 'r') as f:
+        for line in f:
+            if 'INFO       : idx=' in line:
+                print(f"Parsing line: {line.strip()}")
+                idx = int(line.split('idx=')[1].split(':')[0])
+                sim = float(line.split('sim=')[1].split(' ')[0])
+                ttf = float(line.split('ttf=')[1].split(' ')[0]) * 1000 # convert to milliseconds
+                time = float(line.split('time=')[1].split(' ')[0]) * 1000 # convert to milliseconds
+                ntok_0 = int(line.split('ntok=')[1].split(' ')[0].strip('(),'))
+                ntok_1 = int(line.split('ntok=')[1].split(' ')[1].strip('(),'))
+                ntok = (ntok_0, ntok_1)
+                itok_sec = float(line.split('itok/sec=')[1].split(' ')[0].strip('(),'))
+                otok_sec = float(line.split('otok/sec=')[1].split(' ')[0].strip('(),'))
+                otok_rtsec = float(line.split('otok/rtsec=')[1].split(' ')[0].strip('(),'))
+                yield {
+                    'idx': idx,
+                    'sim': sim,
+                    'ttf': ttf,
+                    'time': time,
+                    'ntok': ntok,
+                    'itok_sec': itok_sec,
+                    'otok_sec': otok_sec,
+                    'otok_rtsec': otok_rtsec
+                }
+
+# add file path as argument, using argparse
+import argparse
+
+parser = argparse.ArgumentParser()
+# log files are listed as empty separated string
+parser.add_argument(
+    '--log_files',
+    nargs='+',
+    help='Paths to the log files',
+    default=['provider0.log', 'provider1.log', 'provider2.log'],
+)
+args = parser.parse_args()
+
+log_files = args.log_files
+
+all_providers_metrics = defaultdict()
+for log_file in log_files:
+    metrics = defaultdict(list)
+    for metric in get_metrics_from_log_file(log_file):
+        print(metric)
+        metrics['ttf'].append(metric['ttf'])
+        metrics['time'].append(metric['time'])
+        metrics['otok_sec'].append(metric['otok_sec'])
+        metrics['otok_rtsec'].append(metric['otok_rtsec'])
+    all_providers_metrics[log_file] = metrics
+
+# plot all provider's time metric in same plot, x axis is percentile, y axis is time in milliseconds
+for provider_name, metrics in all_providers_metrics.items():
+    time_values = [np.percentile(metrics['time'], p) for p in range(0, 101, 5)]
+    plt.plot([p for p in range(0, 101, 5)], time_values, label=provider_name)
+    
+plt.xlabel('Percentile')
+plt.ylabel('Time (ms)')
+plt.title('Time distribution')
+plt.grid(True)
+plt.legend()
+plt.tight_layout()
+plt.show()
+plt.savefig(f'time_distribution_{provider_name}.png')
+
